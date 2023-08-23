19d20b9f909efec1bd050d65e62e03ee9251282a
sime-81 <yapock123@gmail.com>
2023-08-23 22:45:55 +0200

    Début du cour React

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure,
+        recusandae atque, nostrum nihil dicta consequuntur blanditiis
+        accusantium minus, temporibus eos omnis impedit sit reprehenderit
+        tempore. Odit iusto sit corporis vel accusantium, deserunt nulla animi
+        laborum optio reiciendis ipsum magnam nesciunt ducimus excepturi beatae
+        perferendis aliquid eos. Sint doloremque perferendis, distinctio modi
+        autem dolores voluptates similique beatae eius corrupti quasi maxime
+        ipsa eos impedit sit voluptatem facere est excepturi fugit dolor esse
+        accusantium labore adipisci tempore? In quasi nobis, necessitatibus
+        placeat obcaecati aspernatur aut autem molestias laudantium dolores ea
+        recusandae cum ex minus animi iure distinctio veniam maxime quam saepe!
+        Velit?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+        consequuntur odio non aut nemo qui, nobis totam dolorem ipsa esse
+        nesciunt atque reprehenderit quis. Recusandae eaque alias quidem
+        voluptatem rem doloribus, autem labore dolores laboriosam eius provident
+        eos incidunt voluptate est sapiente cum eligendi atque tempora,
+        repudiandae nihil molestias sit pariatur vitae necessitatibus. Iure,
+        tenetur? Ea, accusantium? Natus facere minus maxime beatae, atque amet
+        eum optio vel, libero, voluptatem iusto sit. Nulla nesciunt delectus
+        ipsum asperiores sit. Deleniti, ullam. Odio, cupiditate repellat ipsam
+        magni, dicta possimus reiciendis sit corrupti fugit quos repudiandae
+        esse, sunt odit adipisci. Incidunt similique libero dicta corporis id
+        dignissimos quasi saepe cupiditate ad esse. Fuga, a. Odio ratione
+        suscipit odit vero magnam quo error expedita consequuntur? Quasi, saepe
+        consequatur? Ratione hic impedit officia excepturi repellendus tempore
+        debitis voluptates accusantium blanditiis, dolore eligendi doloremque
+        saepe consequatur quia deleniti. Fuga at non, vitae itaque rerum placeat
+        voluptatem corporis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
